1a23bcd5b58dab044e26664dc7b221b738d4f5f6
Da-Wei Lee <daviddwlee84@gmail.com>
2024-09-19 18:27:04 +0800

    Able to provide a name and proof from given list and verify on the server using just Merkle root hash

--- client/index.js
+++ client/index.js
@@ -6,12 +6,20 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  // https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+
+  const index = Math.floor(Math.random() * niceList.length);
+  const name = niceList[index];
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
-  console.log({ gift });
+  console.log(name, { gift });
 }
 
 main();
